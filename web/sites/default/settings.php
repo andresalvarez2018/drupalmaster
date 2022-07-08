0a11bc5921b03f732ac256e955a4b9d4afc241d2
andres.alvarez <andres.alvarez@esinergia.co>
2022-07-08 02:36:17 -0500

    .

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -783,6 +783,7 @@ $databases['default']['default'] = array(
 $settings['config_sync_directory'] = '../config/sync';
 $settings["file_temp_path"] = "/tmp";
 
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
     include $app_root . '/' . $site_path . '/settings.local.php';
 }
