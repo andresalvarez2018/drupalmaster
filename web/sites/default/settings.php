64fd61dbac976d31f8842289eca0d21acedeffae
andres.alvarez <andres.alvarez@esinergia.co>
2022-07-15 22:42:48 -0500

    ajustes host

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -786,6 +786,3 @@ $settings["file_temp_path"] = "/tmp";
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
     include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$settings['trusted_host_patterns'] = [
-    '*',
-];
